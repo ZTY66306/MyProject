17ea6fcb810f0cddc97ddac30b77e9c0b88ae4d3
ZTY66306 <ZTY66306@example.com>
2021-09-18 16:39:55 +0800

    "master 代码"

--- internet/TcpClient.java
+++ internet/TcpClient.java
@@ -3,14 +3,16 @@ package com.heima.internet;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.Properties;
 
 public class TcpClient {
     public static void main(String[] args) throws IOException {
-//        创建一个socket对象
+//        创建一个socket对象   socket.shutdown是用来传递一个结束的标记
         Socket socket = new Socket("127.0.0.1",9999);
 
         OutputStream os= socket.getOutputStream();
         os.write("hello".getBytes());
+
 
         os.close();
         socket.close();
